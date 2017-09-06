5313baefaa23259473b2f6daaeec14ef847a39d0
Balázs Galántai <balazs@platform.sh>
2017-09-06 13:57:06 +0200

    add http to listen url

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -17,7 +17,7 @@ namespace WebApplication1
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .UseApplicationInsights()
-                .UseUrls("127.0.0.1:"+ (Environment.GetEnvironmentVariable("PORT") ?? "5000"))
+                .UseUrls("http://127.0.0.1:"+ (Environment.GetEnvironmentVariable("PORT") ?? "5000"))
                 .Build();
 
             host.Run();
